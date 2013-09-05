80104e1c555660fb3a1f22cc211f8c39a67de200
Alex Takitani <aftakitani@gmail.com>
2013-09-05 14:08:27 -0300

    boleto

---
+++ deploy.rb
@@ -0,0 +1,3 @@
+git add .
+git commit -am "boleto"
+git push
--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -141,7 +141,7 @@ module Brcobranca
           doc.show boleto.codigo_barras.linha_digitavel, :tag => :grande
           doc.moveto :x => '0.5 cm' , :y => '23 cm'
           doc.show boleto.cedente
-          doc.moveto :x => '0.7 cm' , :y => '23 cm'
+          doc.moveto :x => '1.0 cm' , :y => '23 cm'
           doc.show "na casa do pedrinho"
           doc.moveto :x => '11 cm' , :y => '23 cm'
           doc.show boleto.agencia_conta_boleto
